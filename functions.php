326fcc5d644dcc9135df21a145ef6206d1d5f7f6
wts-thomas <thomas@wtsks.com>
2022-10-07 14:27:57 -0500

    Enqueue commented out script removal

--- functions.php
+++ functions.php
@@ -148,7 +148,6 @@ function add_theme_enqueues() {
 	wp_deregister_script('jquery');
 	wp_enqueue_script( 'jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js', array(), '3.6.1', false);
 	wp_enqueue_script( 'viewportHeight', get_template_directory_uri() . '/js/viewportHeight.js#asyncload', array ( 'jquery' ), 1, true);
-   // wp_enqueue_script( 'lazyload', get_template_directory_uri() . '/js/lazy-load.js#asyncload', array ( 'jquery' ), 1, true);
    wp_enqueue_script( 'lazyload-min', get_template_directory_uri() . '/js/lazyload.min.js', array ( 'jquery' ), 1, false);
 }
 add_action( 'wp_enqueue_scripts', 'add_theme_enqueues' );
