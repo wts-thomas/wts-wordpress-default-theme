1addaa2eae8bcdf709f1a23e2f5341c3ed06d193
wts-thomas <thomas@wtsks.com>
2024-03-26 11:11:33 -0500

    TinyMCE function location

--- js/adminScripts.js
+++ js/adminScripts.js
@@ -13,3 +13,26 @@ document.addEventListener('DOMContentLoaded', function() {
    });
  });
  
+ (function() {
+   tinymce.create('tinymce.plugins.MyCustomPlugin', {
+       init : function(ed, url) {
+           ed.addButton('my_custom_class', {
+               title : 'Add custom ID',
+               icon: 'dashicon dashicons-admin-generic', // Using a dashicon
+               onclick : function() {
+                   var selectedText = ed.selection.getContent();
+                   var elementId = prompt("Enter the ID", "");
+
+                   if (elementId != null && elementId != '') {
+                       // Ensure the ID is applied to a block element (like a div or span with display block)
+                       ed.execCommand('mceReplaceContent', false, '<span id="' + elementId + '" style="display:block;">' + selectedText + '</span>');
+                   }
+               }
+           });
+       },
+       createControl : function(n, cm) {
+           return null;
+       },
+   });
+   tinymce.PluginManager.add('my_custom_script', tinymce.plugins.MyCustomPlugin);
+})();
